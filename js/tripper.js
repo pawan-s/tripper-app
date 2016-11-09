2368bf9541c7bdae15b0d4f9cdd0262f25b33a87
pawan-s <lordprometheus@Lords-MacBook-Pro.local>
2016-11-09 17:27:04 -0500

    add filter.html fix link

--- js/tripper.js
+++ js/tripper.js
@@ -13,4 +13,8 @@ var view2 = myApp.addView('#view-2', {
 var view3 = myApp.addView('#view-3');
 var view4 = myApp.addView('#view-4');
 var view5 = myApp.addView('#view-5');
+var view6 = myApp.addView('#view-6', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
 
