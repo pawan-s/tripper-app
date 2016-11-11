fac0a1328c178327aab7a5776f47411701e7f034
pawan-s <lordprometheus@Lords-MacBook-Pro.local>
2016-11-11 13:52:19 -0500

    fix navbar 1

--- js/tripper.js
+++ js/tripper.js
@@ -19,12 +19,25 @@ var view2 = myApp.addView('#view-2', {
 var view3 = myApp.addView('#view-3');
 var view4 = myApp.addView('#view-4');
 var view5 = myApp.addView('#view-5');
+
+// var filter = myApp.addView('#filter', {
+// 	dynamicNavbar: true
+// });
+
+// var filter = myApp.addView('filter', {
+//     // Because we use fixed-through navbar we can enable dynamic navbar
+//     dynamicNavbar: true
+// });
 // var view6 = myApp.addView('#view-6', {
 //     // Because we use fixed-through navbar we can enable dynamic navbar
 //     dynamicNavbar: true
 // });
 
-$$('#link-filter').on('click', function (e) {
+// $$('#link-filter').on('click', function (e) {
     
-    view2.router.loadPage('filter.html');
-});
+//     view2.router.loadPage('filter.html');
+// });
+
+// myApp.onPageInit('about', function (page) {
+//   // Do something here for "about" page
+// })
