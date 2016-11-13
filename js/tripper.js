2c6099854bd873e4b4ad3c042378835c5648264c
pawan-s <lordprometheus@Lords-MacBook-Pro.local>
2016-11-13 14:45:29 -0500

    add images fix day nav bar add swipe day trip

--- js/tripper.js
+++ js/tripper.js
@@ -5,12 +5,6 @@ var myApp = new Framework7();
 // Export selectors engine
 var $$ = Dom7;
 
-// Add views
-// var mainView = myApp.addView('.searchView', {
-//   // Because we want to use dynamic navbar, we need to enable it for this view:
-//   dynamicNavbar: true
-// })
-
 var view1 = myApp.addView('#view-1');
 var view2 = myApp.addView('#view-2', {
     // Because we use fixed-through navbar we can enable dynamic navbar
@@ -20,24 +14,10 @@ var view3 = myApp.addView('#view-3');
 var view4 = myApp.addView('#view-4');
 var view5 = myApp.addView('#view-5');
 
-// var filter = myApp.addView('#filter', {
-// 	dynamicNavbar: true
-// });
-
-// var filter = myApp.addView('filter', {
-//     // Because we use fixed-through navbar we can enable dynamic navbar
-//     dynamicNavbar: true
-// });
-// var view6 = myApp.addView('#view-6', {
-//     // Because we use fixed-through navbar we can enable dynamic navbar
-//     dynamicNavbar: true
-// });
-
-// $$('#link-filter').on('click', function (e) {
-    
-//     view2.router.loadPage('filter.html');
-// });
-
-// myApp.onPageInit('about', function (page) {
-//   // Do something here for "about" page
-// })
+// var mySwiper = myApp.swiper('.swiper-container', {
+//   pagination: '.swiper-pagination',
+//   paginationHide: false,
+//   paginationClickable: true,
+//   nextButton: '.swiper-button-next',
+//   prevButton: '.swiper-button-prev',
+// }); 
