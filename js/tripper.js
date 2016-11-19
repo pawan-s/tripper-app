4743515f4457569c63f09fa8e2f6d07c55838444
pawan-s <lordprometheus@Lords-MacBook-Pro.local>
2016-11-19 14:32:00 -0500

    test drop down

--- js/tripper.js
+++ js/tripper.js
@@ -13,6 +13,35 @@ var view2 = myApp.addView('#view-2', {
 var view3 = myApp.addView('#view-3');
 var view4 = myApp.addView('#view-4');
 var view5 = myApp.addView('#view-5');
+
+//action sheet buttons
+$$('.ac-1').on('click', function () {
+    var buttons = [
+        {
+            text: 'Choose from library',
+            bold: true
+        },
+        {
+            text: 'Take photo'
+        },
+        {
+            text: 'Cancel',
+            color: 'red'
+        },
+    ];
+    myApp.actions(buttons);
+});
+
+var pickerDevice = myApp.picker({
+    input: '#picker-device',
+    cols: [
+        {
+            textAlign: 'center',
+            values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+        }
+    ]
+});
+
  
 // javascript for createtrip.html -> to switch toolbar
 function toggle_visibility(id1,id2) {
@@ -28,3 +57,4 @@ function toggle_visibility(id1,id2) {
 		e2.style.display = 'none';
 	}
 }
+
