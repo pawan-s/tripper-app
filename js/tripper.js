9f660cbff9eb72c7efb81041206b9be2ae23a2cd
pawan-s <lordprometheus@Lords-MacBook-Pro.local>
2016-11-27 21:58:12 -0500

    fix minor bugs

--- js/tripper.js
+++ js/tripper.js
@@ -13,32 +13,6 @@ var view2 = myApp.addView('#view-2', {
 var view3 = myApp.addView('#view-3');
 var view4 = myApp.addView('#view-4');
 var view5 = myApp.addView('#view-5');
-
-
-// date select from calendar
-// var calendarRange = myApp.calendar({
-//     input: '#calendar-range',
-//     dateFormat: 'M dd yyyy',
-//     rangePicker: true
-// });
-
-// //action sheet buttons
-// $$('.ac-1').on('click', function () {
-//     var buttons = [
-//         {
-//             text: 'Choose from library',
-//             bold: true
-//         },
-//         {
-//             text: 'Take photo'
-//         },
-//         {
-//             text: 'Cancel',
-//             color: 'red'
-//         },
-//     ];
-//     myApp.actions(buttons);
-// });
  
 // javascript for createtrip.html -> to switch toolbar
 function toggle_visibility(id1,id2) {
@@ -55,6 +29,7 @@ function toggle_visibility(id1,id2) {
 	}
 }
 
+// javascript for createtrip.html -> simulate trip creation process
 function toggle_time() {
 	var e = document.getElementById('tripTime');
 	if(e.style.display == 'none'){
